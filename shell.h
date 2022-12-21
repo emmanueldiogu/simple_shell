f369763552e3b41728394007855cba28662b75bf
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-12-21 07:13:11 +0100

    extras

--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -36,18 +36,6 @@
 
 extern char **environ;
 
-int _putchar(char c);
-void _puts(char *str);
-int _strlen(char *s);
-char *_strdup(char *str);
-char *concat_all(char *name, char *sep, char *value);
-
-char **splitstring(char *str, const char *delim);
-void execute(char **argv);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
-
-extern char **environ;
 
 /**
  * struct liststr - singly linked list
